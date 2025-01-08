dda559e6dba2a26725bf95082810a3551a2d9f58
tonipekkala24 <tpekkala24@students.oamk.fi>
2025-01-08 10:24:30 +0200

    added fizzbuzz function

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,5 +1,18 @@
 const sum = (num1, num2) => {
     console.log(num1+num2) 
 }
-
 sum(2,5)
+
+const fizzbuzz = () => {
+    for (let i = 0; i < 50; i++) {
+        if (i % 5 == 0) {
+            console.log("fizzbuzz")
+        } else if (i % 3 == 0) {
+            console.log("fizz")
+        } else if (i % 2 == 0) {
+            console.log("buzz")
+        }
+    }
+}
+
+fizzbuzz()
